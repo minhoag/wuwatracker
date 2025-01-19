134faf5a1f5d9ddd31a3b3a4a338e8a9e222a95c
root <wandaandita2003@gmail.com>
2025-01-19 15:33:11 +0700

    change: adjust css for code block. The code block will not break to next line on mobile screen
    DONE #1: add a form validation on StepTwo, where Input will now have error indication base on actionData get from action().
    
    DONE #2: successfully get Checkbox value, to indicate whether use want to share their data to the common database. Should default YES?

--- app/routes/import/stepper.import.tsx
+++ app/routes/import/stepper.import.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import { CopyButton } from '@/components/ui/copy-button';
 import { Input } from '@/components/ui/input';
-import { Link } from '@remix-run/react';
+import { action } from '@/routes/import/route';
+import { Link, useActionData } from '@remix-run/react';
 import { DownloadIcon } from 'lucide-react';
 import * as React from 'react';
 
@@ -24,33 +26,31 @@ export const Stepper = ({ step }: StepperProps) => {
       tabIndex={0}
       className="mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
     >
-      <form>
-        <ol className="relative ms-3 mt-4 border-s">
-          {step.map((step: Step, index: number) => (
-            <li key={'stepper' + index} className="mb-10 ms-8">
-              <span className="absolute -start-4 flex h-8 w-8 items-center justify-center rounded-full bg-accent p-3">
-                <p className="text-sm">{index + 1}</p>
-              </span>
-              <h3 className="mb-1 flex items-center text-base font-semibold text-foreground md:text-lg">
-                {step.title}
-              </h3>
-              <p className="mb-4 text-sm font-normal text-muted-foreground md:text-base">
-                {step.description}
-              </p>
-              {step.render}
-            </li>
-          ))}
-        </ol>
-        <div className="grid items-center gap-4 md:flex md:justify-end">
-          <button
-            className="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground hover:bg-primary/90 h-11 rounded px-8"
-            type="submit"
-          >
-            <DownloadIcon className="!h-4 !w-4" />
-            Import History
-          </button>
-        </div>
-      </form>
+      <ol className="relative ms-3 mt-4 border-s">
+        {step.map((step: Step, index: number) => (
+          <li key={'stepper' + index} className="mb-10 ms-8">
+            <span className="absolute -start-4 flex h-8 w-8 items-center justify-center rounded-full bg-accent p-3">
+              <p className="text-sm">{index + 1}</p>
+            </span>
+            <h3 className="mb-1 flex items-center text-base font-semibold text-foreground md:text-lg">
+              {step.title}
+            </h3>
+            <p className="mb-4 text-sm font-normal text-muted-foreground md:text-base">
+              {step.description}
+            </p>
+            {step.render}
+          </li>
+        ))}
+      </ol>
+      <div className="grid items-center gap-4 md:flex md:justify-end">
+        <Button
+          className="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground hover:bg-primary/90 h-11 rounded px-8"
+          type="submit"
+        >
+          <DownloadIcon className="!h-4 !w-4" />
+          Import History
+        </Button>
+      </div>
     </div>
   );
 };
@@ -61,10 +61,9 @@ export function StepOne() {
       <div className="flex w-full items-center space-x-2">
         <div className="relative w-full" data-state="closed">
           <div className="h-10 flex items-center justify-between border gap-2 rounded-md pl-3 pr-1.5">
-            <code className="text-[13px]">
+            <code className="text-[13px] overflow-x-auto whitespace-nowrap">
               iwr -useb "https://wuwatracker.com/import.ps1" | iex
             </code>
-
             <CopyButton
               value={
                 'iwr -useb "https://wuwatracker.com/import.ps1" | iex'
@@ -103,30 +102,36 @@ export function StepOne() {
 }
 
 export function StepTwo() {
+  const actionData = useActionData<typeof action>();
   return (
-    <div>
-      <Input className="flex h-10 w-full rounded-md px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:ring-none border border-border bg-background font-mono text-xs shadow-inner focus:outline-none dark:shadow-none" />
+    <div className="relative">
+      <Input
+        name="gacha_url"
+        className="flex h-10 w-full rounded-md px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:ring-none border border-border bg-background font-mono text-xs shadow-inner focus:outline-none dark:shadow-none"
+      />
+      {actionData?.errors?.url ? (
+        <p
+          id={Date.now().toString()}
+          className="absolute -bottom-6 left-1 text-[0.8rem] font-medium text-red-400"
+        >
+          {actionData?.errors.url}
+        </p>
+      ) : null}
     </div>
   );
 }
 
 export function StepTwoTitle() {
   return (
-    <span className="flex flex-row gap-x-2 items-center">
+    <span className="flex flex-col gap-x-2 md:flex-row">
       Dán link của bạn vào đây.
-      <span className="shortcut text-muted-foreground">
-        Sử dụng phím tắt
-      </span>
-      <span className="text-sm text-muted-foreground">
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-sm font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">Ctrl + V </span>
-        </kbd>
-      </span>{' '}
-      hoặc{' '}
-      <span className="text-sm text-muted-foreground">
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-sm font-medium text-muted-foreground opacity-100">
-          <span className="text-sm mr-0.5">⌘</span>V
-        </kbd>
+      <span className="flex gap-x-2 items-center shortcut mt-1 text-sm text-muted-foreground">
+        Gợi ý: sử dụng phím tắt
+        <span className="text-sm text-muted-foreground">
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-sm font-medium text-muted-foreground opacity-100">
+            <span className="text-xs">Ctrl + V </span>
+          </kbd>
+        </span>
       </span>
     </span>
   );
@@ -136,7 +141,7 @@ export function StepThree() {
   return (
     <div>
       <div className="flex items-center space-x-2">
-        <Checkbox id="terms-controlled" />
+        <Checkbox name="share" id="terms-controlled" />
         <label
           htmlFor="terms-controlled"
           className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
